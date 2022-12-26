83288bd98ab5e1f7b7e8b00533833a25a25387ff
NkBear <113137841+NkBear@users.noreply.github.com>
2022-12-26 13:52:04 +0300

    Update 19.jl

--- 19.jl
+++ 19.jl
@@ -1,21 +1,28 @@
 using HorizonSideRobots
 
-function nomber19!(robot)
-    recursivemarker!(robot,Nord)
-end
-
-function recursivemarker!(robot,side)
-    n=0
-    move!(robot,side)
-    if !isborder(robot,side)
-        recursivemarker!(robot,side)
-        n+=1
-    else
-        putmarker!(robot)
+function num_borders(r::Robot)
+    side = Nord
+    num=0
+    while isborder(r,Ost)==false
+        while isborder(r,side)==false
+            if pass_the_borders(r,side) == true
+                num += 1
+                walk_by(r,side)
+            end
+        end
+        move!(r,Ost)
+        side = inverse(side)
     end
-    for _ in 1:n
-        move!(robot,inverse(side))
-    end
+    return num
 end
-
-inverse(side::HorizonSide)=HorizonSide((Int(side)+2)%4)
+function pass_the_borders(r::Robot,side::HorizonSide) 
+    while isborder(r,Ost)==false 
+        if isborder(r,side) == true
+            return false
+        end
+        move!(r,side) 
+    end
+    return true
+end
+walk_by(r::Robot,side::HorizonSide) = while isborder(r,Ost)==true move!(r,side) end
+inverse(side::HorizonSide)=HorizonSide(mod(Int(side)+2,4))
